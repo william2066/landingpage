69588d2d14ebcc3fd383f730cc5621ded2af6917
william2066 <66227375+william2066@users.noreply.github.com>
2022-05-12 22:23:47 -0300

    tentantiva

---
+++ scriptjs.js
@@ -0,0 +1,17 @@
+console.log("JS CARREGADO.....");
+
+
+function carburado(){
+
+    console.log('carburado');
+
+    //window.location.href='https://youtu.be/Be8Y7UmiHNA?t=11';
+    window.open('https://youtu.be/Be8Y7UmiHNA?t=11');
+    
+}
+
+
+function narutosad(){
+
+    window.open('https://youtu.be/1ZgdYri3H4Q?t=12');
+}
